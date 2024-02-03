0bb7a5cf44c76703c9a3fa8fa718148408c892cb
amirrr1987 <maghami.a1987@gmail.com>
2024-02-03 21:52:59 +0330

    feat: create user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { el } from "araz"
+import { el } from "araz";
+import axios from "axios";
+
+const getUsers = async () => {
+  try {
+    const { data } = await axios.get("https://jsonplaceholder.org/users");
+    const parent = document.getElementById("user-list");
+    while (parent?.firstChild) {
+      parent.firstChild.remove();
+    }
+    data.map((item: any) => {
+      parent?.append(
+        el("div", { class: "bg-white p-4 rounded" }, item.firstname + " " + item.lastname)
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const AppComponent = () => {
-    return el(
+  return el(
+    "section",
+    {
+      class: "bg-blue-50 py-12",
+      events: {
+        onclick: getUsers,
+      },
+    },
+    el(
       "div",
       {
-        class: "bg-red py-12 grid grid-cols-2 gap-4 p-4",
+        class: "container mx-auto px-4 grid gap-4",
       },
-        [el("p", {
-          class: "bg-yellow py-4"
-      }, "sdf"), el("p", {}, "sdf")]
-    );
-}
+      [
+        el(
+          "div",
+          {},
+          el(
+            "button",
+            { class: "px-6 py-2 bg-blue text-light rounded" },
+            "Get Users"
+          )
+        ),
+        el("div", {
+          id: "user-list",
+          class: "grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6",
+        }),
+      ]
+    )
+  );
+};
